8056419643549432c98ba9b87e65358b702d7e44
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2014-02-28 23:26:45 +0000

    Initial commit

---
+++ prac1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prac1::Application.config.secret_key_base = 'd50625283fdffded466404781120808e7c2d7d144de3a91b06aec41da5c19d0c6197dd4c9a4eea96fb7bf233fea0c338b75d2bceccc292535aeda7a55392d5a8'
